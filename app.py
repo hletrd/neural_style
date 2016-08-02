32eccd32fa3d88809ee1770a46677e405387d941
Jiyong Youn <01@0101010101.com>
2016-08-02 15:33:27 +0900

    Changed to bootstrap & renewal

--- app.py
+++ app.py
@@ -15,16 +15,15 @@ db = dbclient.db_neural
 col = db.images
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '/home/hletrd/neural-run/files/'
+app.config['UPLOAD_FOLDER'] = config.upload_folder
 
-gpu = "-1"
 app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
 processing = False
 t = False
 col.update_many({"queued": False, "status": False}, {"$set": {"queued": True}})
 
 class t_run(threading.Thread):
-	def __init__(self, url, cweight, sweight, tweight, ni):
+	def __init__(self, url, cweight, sweight, tweight, ni, isize):
 		threading.Thread.__init__(self)
 		global processing
 		processing = True
@@ -33,10 +32,11 @@ class t_run(threading.Thread):
 		self.sweight = str(sweight)
 		self.tweight = str(tweight)
 		self.ni = str(ni)
+		self.isize = str(isize)
 
 	def run(self):
 		os.chdir('/home/hletrd/neural-style/')
-		self.p = subprocess.call(["th", "neural_style.lua", "-style_image", app.config['UPLOAD_FOLDER'] + self.url + "_style.jpg", "-content_image", app.config['UPLOAD_FOLDER'] + self.url + "_content.jpg", "-gpu", gpu, "-output_image", app.config['UPLOAD_FOLDER'] + self.url + "_out.png", "-image_size", "384", "-optimizer", "lbfgs", "-content_weight", self.cweight, "-style_weight", self.sweight, "-tv_weight", self.tweight, "-num_iterations", self.ni])
+		self.p = subprocess.call(["th", "neural_style.lua", "-style_image", app.config['UPLOAD_FOLDER'] + self.url + "_style.jpg", "-content_image", app.config['UPLOAD_FOLDER'] + self.url + "_content.jpg", "-gpu", config.gpu, "-output_image", app.config['UPLOAD_FOLDER'] + self.url + "_out.png", "-image_size", self.isize, "-optimizer", "lbfgs", "-backend", config.backend, "-content_weight", self.cweight, "-style_weight", self.sweight, "-tv_weight", self.tweight, "-num_iterations", self.ni])
 		global processing
 		processing = False
 
@@ -49,13 +49,8 @@ def index():
 	<meta charset="utf-8">
 	<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	<title>Neural Style Online</title>
-	<link rel="stylesheet" href="/files/normalize.css">
-	<link rel="stylesheet" href="/files/skeleton.css">
-	<style type="text/css">
-	.u-full-width {
-		height: 30px !important;
-	}
-	</style>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -64,25 +59,67 @@ def index():
   ga('create', 'UA-36880204-4', 'auto');
   ga('send', 'pageview');
 	</script>
+	<style type="text/css">
+	.spacer{
+	height: 10px;
+	}
+	</style>
 </head>
 <body>
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="/">Neural artistic styling</a>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+				<li class="active"><a href="/">Upload new image</a></li>
+				<li><a href="/list">List of uploaded images</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+				<li><a href="https://0101010101.com">Created by HLETRD</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 	<div class="container">
-	<h4>Web-based neural image styling by HLETRD</h4>
-	<form method="POST" action="/submit" enctype="multipart/form-data">
+	<form method="POST" action="/submit" enctype="multipart/form-data" class="form-horizontal">
 	<p>This service is based on <a href="https://github.com/jcjohnson/neural-style/">Torch implementation of neural style algorithm</a> by jcjohnson.</p>
 	<p>Only jpg files are allowed. Maximum allowed size is 8MB totally.</p>
 	<hr>
-	<h5>Select images</h5>
-	<div class="row"><p class="six columns">Select style image</p><input class="six columns" name="style" type="file"></div>
-	<div class="row"><p class="six columns">Select content image</p><input class="six columns" name="content" type="file"></div>
-	<br />
-	<h5>Set optional parameters</h5>
-	<div class="row"><p class="eight columns">Input number of iterations(min: 1, max: 1000): </p><input class="u-full-width four columns" name="ni" type="text" value="300"></div>
-	<div class="row"><p class="eight columns">Input content weight(How much to weight the content reconstruction term.)</p><input class="u-full-width four columns" name="cweight" type="text" value="5"></div>
-	<div class="row"><p class="eight columns">Input style weight(How much to weight the style reconstruction term.)</p><input class="u-full-width four columns" name="sweight" type="text" value="100"></div>
-	<div class="row"><p class="eight columns">Input tv weight(Weight of total-variation (TV) regularization; this helps to smooth the image.)</p><input class="u-full-width four columns" name="tweight" type="text" value="0.001"></div>
+	<strong>Select images</strong>
+	<div class="spacer"></div>
+	<div class="row"><div class="col-md-4">Select style image</div><div class="col-md-8"><input class="form-control" name="style" type="file"></div>
+	<div class="col-md-8 col-md-offset-4">Choose a image with artistic style. Images from famous artists are recommended.<br />(Examples: The Starry Night, The Scream.)</div></div>
+	<div class="spacer"></div>
+	<div class="row"><div class="col-md-4">Select content image</div><div class="col-md-8"><input class="form-control" name="content" type="file"></div>
+	<div class="col-md-8 col-md-offset-4">Choose a image that you want to repaint with artistic style.</div></div>
+	<hr>
+	<strong>Set optional parameters</strong>
+	<div class="spacer"></div>
+	<div class="row"><div class="col-md-4">Input number of iterations</div><div class="col-md-8"><input class="form-control" name="ni" type="text" value="500"></div>
+	<div class="col-md-8 col-md-offset-4">Min: 1, Max: 1000. Increasing the value may increase overall quality of the image.</div></div>
+	<div class="spacer"></div>
+	<div class="row"><div class="col-md-4">Input content weight</div><div class="col-md-8"><input class="form-control" name="cweight" type="text" value="5"></div>
+	<div class="col-md-8 col-md-offset-4">How much to weight the content reconstruction term.</div></div>
+	<div class="spacer"></div>
+	<div class="row"><div class="col-md-4">Input style weight</div><div class="col-md-8"><input class="form-control" name="sweight" type="text" value="100"></div>
+	<div class="col-md-8 col-md-offset-4">How much to weight the style reconstruction term.</div></div>
+	<div class="spacer"></div>
+	<div class="row"><div class="col-md-4">Input tv weight</div><div class="col-md-8"><input class="form-control" name="tweight" type="text" value="0.001"></div>
+	<div class="col-md-8 col-md-offset-4">Weight of total-variation (TV) regularization; this helps to smooth the image.</div></div>
+	<div class="spacer"></div>
+	<div class="row"><div class="col-md-4">Input result image size</div><div class="col-md-8"><input class="form-control" name="isize" type="text" value="512"></div>
+	<div class="col-md-8 col-md-offset-4">Size of long side of the result image in pixels. Max: 1000</div></div>
 	<!--<div class="row"><p class="eight columns">Input learning rate(Learning rate to use with the ADAM optimizer.)</p><input class="u-full-width four columns" name="lrate" type="text" value="1"></div>-->
-	<input class="button-primary" type="submit" value="Submit images">
+	<div class="spacer"></div>
+	<div class="row"><div class="col-md-2 col-md-offset-4"><input class="btn-block btn btn-primary" type="submit" value="Submit images"></div></div>
 	</form>
 	<hr>
 	<ul>
@@ -91,7 +128,6 @@ def index():
 		<li>If your result is hard to recognize, try increasing number of iterations.</li>
 		<li><a href="https://github.com/hletrd/neural_style">This project on GitHub</a></li>
 	</ul>
-	<a href="/list">List of uploaded files</a>
 	</div>
 </body>
 </html>"""
@@ -105,6 +141,11 @@ def submit():
 	tweight = float(request.form['tweight'])
 	#lrate = float(request.form['lrate'])
 	ni = int(request.form['ni'])
+	isize = int(request.form['isize'])
+	if isize > 1024:
+		isize = 1024
+	elif isize < 1:
+		isize = 1
 	if ni > 1000:
 		ni = 1000
 	elif ni < 1:
@@ -114,7 +155,7 @@ def submit():
 		style.save(os.path.join(app.config['UPLOAD_FOLDER'], url + '_style.jpg'))
 		content.save(os.path.join(app.config['UPLOAD_FOLDER'], url + '_content.jpg'))
 		if imghdr.what(os.path.join(app.config['UPLOAD_FOLDER'], url + '_style.jpg')) == 'jpeg' and imghdr.what(os.path.join(app.config['UPLOAD_FOLDER'], url + '_content.jpg')) == 'jpeg':
-			col.insert_one({"url": url, "status": False, "uploaded": strftime("%Y-%m-%d %H:%M:%S", gmtime()), "queued": True, "cweight": cweight, "sweight": sweight, "tweight": tweight, "ni": ni})
+			col.insert_one({"url": url, "status": False, "uploaded": strftime("%Y-%m-%d %H:%M:%S", gmtime()), "queued": True, "cweight": cweight, "sweight": sweight, "tweight": tweight, "ni": ni, "isize": isize})
 			return redirect('/list')
 		else:
 			return '<!doctype HTML><html><head><title>Error</title></head><body>error: not a valid JPEG file.</body></html>'
@@ -151,36 +192,72 @@ def list(page=1):
 	lastpage = math.ceil(1.0 * col.count() / 10)
 	pagelist = ''
 	if page > 5:
-		pagelist += '<a href="/list/' + str(page - 5) + '">' + str(page - 5) + '</a> '
+		pagelist += '<li><a href="/list/' + str(page - 5) + '">' + str(page - 5) + '</a></li>'
 	if page > 4:
-		pagelist += '<a href="/list/' + str(page - 4) + '">' + str(page - 4) + '</a> '
+		pagelist += '<li><a href="/list/' + str(page - 4) + '">' + str(page - 4) + '</a></li>'
 	if page > 3:
-		pagelist += '<a href="/list/' + str(page - 3) + '">' + str(page - 3) + '</a> '
+		pagelist += '<li><a href="/list/' + str(page - 3) + '">' + str(page - 3) + '</a></li>'
 	if page > 2:
-		pagelist += '<a href="/list/' + str(page - 2) + '">' + str(page - 2) + '</a> '
+		pagelist += '<li><a href="/list/' + str(page - 2) + '">' + str(page - 2) + '</a></li>'
 	if page > 1:
-		pagelist += '<a href="/list/' + str(page - 1) + '">' + str(page - 1) + '</a> '
-	pagelist += str(page) + ' '
+		pagelist += '<li><a href="/list/' + str(page - 1) + '">' + str(page - 1) + '</a></li>'
+	pagelist += '<li class="active"><a href="#">' + str(page) + '</a></li>'
 	if lastpage >= page+1:
-		pagelist += '<a href="/list/' + str(page + 1) + '">' + str(page + 1) + '</a> '
+		pagelist += '<li><a href="/list/' + str(page + 1) + '">' + str(page + 1) + '</a></li>'
 	if lastpage >= page+2:
-		pagelist += '<a href="/list/' + str(page + 2) + '">' + str(page + 2) + '</a> '
+		pagelist += '<li><a href="/list/' + str(page + 2) + '">' + str(page + 2) + '</a></li>'
 	if lastpage >= page+3:
-		pagelist += '<a href="/list/' + str(page + 3) + '">' + str(page + 3) + '</a> '
+		pagelist += '<li><a href="/list/' + str(page + 3) + '">' + str(page + 3) + '</a></li>'
 	if lastpage >= page+4:
-		pagelist += '<a href="/list/' + str(page + 4) + '">' + str(page + 4) + '</a> '
+		pagelist += '<li><a href="/list/' + str(page + 4) + '">' + str(page + 4) + '</a></li>'
 	if lastpage >= page+5:
-		pagelist += '<a href="/list/' + str(page + 5) + '">' + str(page + 5) + '</a> '
+		pagelist += '<li><a href="/list/' + str(page + 5) + '">' + str(page + 5) + '</a></li>'
 	return """<!doctype HTML>
-	<html>
-	<head>
-		<meta charset="utf-8">
-		<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-		<title>List</title>
-		<link rel="stylesheet" href="/files/normalize.css">
-		<link rel="stylesheet" href="/files/skeleton.css">
-	</head>
-	<body><div class="container">""" + result + '<hr><span style="font-size: 1.2em">' + pagelist + '</span><hr><a href="/">Back</a>' + """<script>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<title>Neural Style Online</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-36880204-4', 'auto');
+  ga('send', 'pageview');
+	</script>
+	<style type="text/css">
+	.spacer{
+	height: 10px;
+	}
+	</style>
+</head>
+<body>
+	<nav class="navbar navbar-default">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="/">Neural artistic styling</a>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+				<li><a href="/">Upload new image</a></li>
+				<li class="active"><a href="/list">List of uploaded images</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+				<li><a href="https://0101010101.com">Created by HLETRD</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="container">""" + result + '<hr><div class="text-center"><nav><ul class="pagination">' + pagelist + '</ul></nav>' + """<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -194,27 +271,64 @@ def image(url):
 	an_image = col.find_one({"url": url})
 	if an_image:
 		result = """<!doctype HTML>
-		<head>
-			<meta charset="utf-8">
-			<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-			<title>Image</title>
-			<link rel="stylesheet" href="/files/normalize.css">
-			<link rel="stylesheet" href="/files/skeleton.css">
-		</head>
-		<body>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<title>Neural Style Online</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-36880204-4', 'auto');
+  ga('send', 'pageview');
+	</script>
+	<style type="text/css">
+	.spacer{
+	height: 10px;
+	}
+	</style>
+</head>
+<body>
+	<nav class="navbar navbar-default">
 		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar">Image: """ + url + """</span>
+				</button>
+				<a class="navbar-brand" href="/">Neural artistic styling</a>
+			</div>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+				<li><a href="/">Upload new image</a></li>
+				<li><a href="/list">List of uploaded images</a></li>
+				<li class="active"><a href="#"></a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+				<li><a href="https://0101010101.com">Created by HLETRD</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="container">
 		<h4>style</h4>
-		<img alt="" src="/files/""" + url + """_style.jpg" width="512">
+		<img alt="" src="/files/""" + url + """_style.jpg" width="100%">
 		<hr>
 		<h4>content</h4>
-		<img alt="" src="/files/""" + url + """_content.jpg" width="512">
+		<img alt="" src="/files/""" + url + """_content.jpg" width="100%">
 		<hr>
 		<h4>result</h4>
-		<img alt="" src="/files/""" + url + """_out.png" width="512">
+		<img alt="" src="/files/""" + url + """_out.png" width="100%">
 		<hr>"""
 		if 'ni' in an_image:
 			result += "<h5>Number of iterations: " + str(an_image['ni']) + "</h5><h5>Content weight: " + str(an_image['cweight']) + "</h5><h5>Style weight: " + str(an_image['sweight']) + "</h5><h5>Tv weight: " + str(an_image['tweight']) + "</h5><hr>"
-		result += """<a class="button-primary" href="#" onclick="history.go(-1)">Back</a>
+		result += """<a class="btn btn-primary" href="#" onclick="history.go(-1)">Back</a>
 		<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -235,20 +349,20 @@ def staticfile(path):
 	return send_from_directory(app.config['UPLOAD_FOLDER'], path, as_attachment=False)
 
 def timer():
-	threading.Timer(3.0, timer).start()
+	#threading.Timer(3.0, timer).start()
 	global processing
 	if processing == False:
 		try:
 			a = col.find({"queued": True}).sort("uploaded", 1).limit(1).next()
 			if a:
 				global t
-				t = t_run(a['url'], a['cweight'], a['sweight'], a['tweight'], a['ni'])
+				t = t_run(a['url'], a['cweight'], a['sweight'], a['tweight'], a['ni'], a['isize'])
 				t.start()
 				col.update({"url": a['url']}, {"$set": {"queued": False, "pstarted": strftime("%Y-%m-%d %H:%M:%S", gmtime())}}, upsert=False)
 		except:
 			a = 1
 
-threading.Timer(3.0, timer).start()
+#threading.Timer(3.0, timer).start()
 
 if __name__ == '__main__':
-	app.run(debug=True, host="0.0.0.0", port=9002, use_reloader=False)
+	app.run(debug=True, host="0.0.0.0", port=9002, use_reloader=True)
